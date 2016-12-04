6184dcd8ca1c147b3d31ff23b9af7675c7140c2c
Cristian Umaña <cumanacr@gmail.com>
2016-12-04 17:00:07 -0600

    feat: touch gulpfile.js and basic examples

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+// Styles
+gulp.task('styles', function(){
+    console.log('starting styles task');
+});
+
+// Scripts
+
+// Images
+
+// Default
+gulp.task('default', function(){
+    console.log('starting default task');
+});
