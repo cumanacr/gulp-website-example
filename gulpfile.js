33171256d51115657e0c79efc4c6be83ce5819bb
Cristian Umaña <cumanacr@gmail.com>
2016-12-04 17:01:57 -0600

    feat: new gulp functions

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,14 @@ gulp.task('styles', function(){
 });
 
 // Scripts
+gulp.task('scripts', function(){
+    console.log('starting scripts task');
+});
 
 // Images
+gulp.task('images', function(){
+    console.log('starting images task');
+});
 
 // Default
 gulp.task('default', function(){
