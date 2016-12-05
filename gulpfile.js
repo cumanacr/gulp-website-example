27d1a07bd5f363117a39ee3cbd5b941460464265
Cristian Umaña <cumanacr@gmail.com>
2016-12-05 15:46:59 -0600

    feat: gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
 // File paths
 var DIST_PATH = 'public/dist';
@@ -14,6 +15,11 @@ var CSS_PATH ='public/css/**/*.css';
 gulp.task('styles', function() {
     console.log('starting styles task');
     return gulp.src(['public/css/reset.css', CSS_PATH])
+        .pipe(plumber(function (err) {
+            console.log('Styles Task Error');
+            console.log(err);
+            this.emit('end');
+        }))
         .pipe(autoprefixer(/*{browsers: ['last 2 versions', 'ie 8']}*/))
         .pipe(concat('styles.css'))
         .pipe(minifyCss())
