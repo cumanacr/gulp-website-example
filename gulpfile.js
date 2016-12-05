7a6102a1e4e658fb8211bc55f6ac268eacb57f2d
Cristian Umaña <cumanacr@gmail.com>
2016-12-05 14:22:16 -0600

    feat: gulp-concat working

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
 
 // File paths
+var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH ='public/css/**/*.css';
 
 // Styles
 gulp.task('styles', function() {
     console.log('starting styles task');
+    return gulp.src(['public/css/reset.css', CSS_PATH])
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(DIST_PATH))
+        .pipe(livereload());
 });
 
 // Scripts
@@ -15,7 +22,7 @@ gulp.task('scripts', function() {
     console.log('starting scripts task');
     return gulp.src(SCRIPTS_PATH)
         .pipe(uglify())
-        .pipe(gulp.dest('public/dist'))
+        .pipe(gulp.dest(DIST_PATH))
         .pipe(livereload());
 });
 
